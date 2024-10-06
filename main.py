cbc89a255e4e23717e0196cea2c993c7829eda06
Tata Marijan <tata.marijan@gmail.com>
2024-10-06 18:40:34 +0200

    Count Words - my solution

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+
+def main():
+    path_to_file = "./books/frankenstein.txt"
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        print(num_of_words(file_contents))
+
+
+# my solution    
+def num_of_words(book):
+    words = book.split() # line below creates a list of words from the book    
+    return len(words) # line below returns simple list number of elements
+
+#bd solution
+
+main()
