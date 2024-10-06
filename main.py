38796c75877a8f08f41e8ca0bac8c94460faf63a
Tata Marijan <tata.marijan@gmail.com>
2024-10-06 18:44:38 +0200

    CountWord my+bd solutions

--- main.py
+++ main.py
@@ -12,5 +12,24 @@ def num_of_words(book):
     return len(words) # line below returns simple list number of elements
 
 #bd solution
+'''
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
 
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
+'''
 main()
